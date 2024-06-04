428f27fc70b04425089ad1afe5edb3cbb0f80872
Jorge Guiza <jorgeluis.gg.2000@gmail.com>
2024-06-04 10:25:07 -0500

    Chore: Default allow host anyone.

--- drf/envs/MainEnv.py
+++ drf/envs/MainEnv.py
@@ -15,7 +15,7 @@ class MainEnv:
             SECRET_KEY=(
                 str, 'django-insecure-=!+2=2-^&gw71@%xfj&)%3&jn8b^u-t^8r-i^!y5rms7+tu50g'),
             DATABASE_URL=(str, 'sqlite:///db.sqlite3'),
-            ALLOWED_HOSTS=(list, ['127.0.0.1', 'localhost'])
+            ALLOWED_HOSTS=(list, [])
         )
         environ.Env.read_env()
         self.DEBUG = self.__env('DEBUG')
